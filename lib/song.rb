960496bc490200b7fe37e1a355b9505d6cf2cbec
Eric Storey <estorey@gmail.com>
2019-03-31 17:36:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song=self.new(filename.split(" - ")[1])
+    song=self.(filename.split(" - ")[1])
     song.artist= Artist.find_or_create_by_name(filename.split(" - ")[0])
     song.artist.add_song(song)
     song
